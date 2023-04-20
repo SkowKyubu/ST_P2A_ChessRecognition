bbad95624911374c4d7ab95a4d2d8cbb8e1dddcc
SkowKyubu <120100522+SkowKyubu@users.noreply.github.com>
2023-04-20 14:20:27 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,8 @@
+# Libraries
+import streamlit as st
+
+# Confit
+st.set_page_config(page_title='Cross Chain Monitoring Tool', page_icon=':bar_chart:')
+
+# Title
+st.title("Application de reconnaissance d'image par des réseaux de neurones")
