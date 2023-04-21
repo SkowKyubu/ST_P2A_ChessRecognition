bb093c2d6e621a2d02eeec387e8af1779c01bb72
SkowKyubu <120100522+SkowKyubu@users.noreply.github.com>
2023-04-21 16:18:59 +0200

    Update 2_camera.py

--- pages/2_camera.py
+++ pages/2_camera.py
@@ -7,11 +7,6 @@ import chess.svg
 import model_yolo2
 import ChessBoard
 import fonction_annexe
-import av
-import threading
-from typing import Union
-
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 # Config
 st.set_page_config(page_title="Reconnaissance : caméra", page_icon=':bar_chart:')
@@ -20,67 +15,53 @@ st.set_page_config(page_title="Reconnaissance : caméra", page_icon=':bar_chart:
 st.title("Reconnaissance de parties d'échec en temps réel")
 # ======================================================================================================================
 
+# Initialisez une variable pour stocker l'état de la case à cocher
+checkbox_state = st.checkbox("Activer la caméra")
+
 # Define a boolean flag to keep track of the model loading status
 model_loaded = False
 if not model_loaded:
     model = model_yolo2.load_yolo_model('best4.pt',version=5)
     model_loaded = True
 
-FRAME_WINDOW = st.empty()
-
-def main():
-    class VideoTransformer(VideoTransformerBase):
-        frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
-        in_image: Union[np.ndarray, None]
-        out_image: Union[np.ndarray, None]
-
-        def __init__(self) -> None:
-            self.frame_lock = threading.Lock()
-            self.in_image = None
-            self.out_image = None
-
-        def transform(self, frame: av.VideoFrame) -> np.ndarray:
-            in_image = frame.to_ndarray(format="bgr24")
-
-            out_image = in_image[:, ::-1, :]  # Simple flipping for example.
-
-            with self.frame_lock:
-                self.in_image = in_image
-                self.out_image = out_image
-
-            return out_image
-
-    ctx = webrtc_streamer(key="snapshot", video_transformer_factory=VideoTransformer)
-
-    if ctx.video_transformer:
-        if st.button("Snapshot"):
-            with ctx.video_transformer.frame_lock:
-                in_image = ctx.video_transformer.in_image
-                out_image = ctx.video_transformer.out_image
-
-            if in_image is not None and out_image is not None:
-                st.write("Output image:")
-                st.image(out_image, channels="BGR")
-            # Partie liée à la détection :
-                # Load the YOLO model only if it has not been loaded before
-                boxes = model_yolo2.get_yolo_prediction(out_image, model)
-                placements = model_yolo2.get_boxes_position(boxes)
-                all_corners_in_chessboard, t = ChessBoard.get_chess_board(out_image)
-                fen = fonction_annexe.get_placement(t, placements)
-
-                # chessboard
-                board = chess.Board(fen)
-                img_chess = chess.svg.board(
-                    board,
-                    size=500)
-
-                FRAME_WINDOW.write(
-                    f'<div style="display: flex; justify-content: center; padding-bottom: 50px ;">{img_chess}</div>',
-                    unsafe_allow_html=True)
-            else:
-                st.warning("No frames available yet.")
 
 
-if __name__ == "__main__":
-    main()
+# Créez un bouton qui active/désactive la case à cocher
+if checkbox_state:
+    FRAME_WINDOW = st.empty()
+    camera = cv2.VideoCapture(0)
+    camera.set(3, 1280)  # Résolution de l'affichage :
+    camera.set(4, 720)
 
+    while checkbox_state:
+        _, image = camera.read()
+        # frame = model_yolo.run_yolo(frame)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Partie liée à la détection :
+        if image is not None:
+            # Load the YOLO model only if it has not been loaded before
+            boxes = model_yolo2.get_yolo_prediction(image, model)
+            placements = model_yolo2.get_boxes_position(boxes)
+            all_corners_in_chessboard, t = ChessBoard.get_chess_board(image)
+            fen = fonction_annexe.get_placement(t, placements)
+
+            # chessboard
+            board = chess.Board(fen)
+            img_chess = chess.svg.board(
+                board,
+                size=500)
+
+            FRAME_WINDOW.write(f'<div style="display: flex; justify-content: center; padding-bottom: 50px ;">{img_chess}</div>',
+                     unsafe_allow_html=True)
+
+
+
+
+        time.sleep(5) # Pause de 10 secondes avant de lire la prochaine image
+        # Vérifier si la case à cocher a été décochée
+        if not checkbox_state:
+            # Sortir de la boucle et arrêter la capture vidéo
+            camera.release()
+            break
+            # Sortir de la boucle et arrêter la capture vidéo
